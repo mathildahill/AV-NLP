a23137cabb23fac0c212343a994c21746f639ae9
mathildahill <mathilda.hill@gmail.com>
2023-09-21 14:23:26 +0100

    Create script to clean data

---
+++ src/data_cleaning.py
@@ -0,0 +1,18 @@
+
+
+import re
+import nltk
+import string
+import warnings
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option("display.max_colwidth", 200)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+%matplotlib inline
+
+train = pd.read_csv('data/train_E6oV31V.csv')
+test = pd.read_csv('test_tweets_anuFYb8.csv')
